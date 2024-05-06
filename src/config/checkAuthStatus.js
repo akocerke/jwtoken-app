eec6ab1a33011e31fff7451c4f3525ca107d5167
akocerke <152269613+akocerke@users.noreply.github.com>
2024-05-06 14:37:42 +0200

    update

--- src/config/checkAuthStatus.js
+++
@@ -1,9 +0,0 @@
-// checkAuthStatus.js
-export const checkAuthStatus = () => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      console.log('Benutzer ist angemeldet.');
-    } else {
-      console.log('Benutzer ist nicht angemeldet.');
-    }
-  };
